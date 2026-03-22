350f54b5426bdfb15e74570148c495e65c725008
Nivekyareg17 <guevaraclavijok@gmail.com>
2026-03-22 15:55:28 -0500

    update_theme

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+   //Brand colors green
+    static const Color primaryGreen = Color(0xFF059669);
+    static const Color secondaryGreen = Color(0xFF25F46A);
+    static const Color darkGreen = Color(0xFF064E3B);
+    static const Color ligthGreen = Color(0xFF00FF94);
+
+}
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  //Light theme
+    static ThemeData LightTheme = ThemeData(
+    
+    );
+//Dark theme
+    static ThemeData DarkTheme = ThemeData(
+    
+    );
+}
